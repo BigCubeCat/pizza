1bc3b52902a6da90df6667778c474c18ad881cc4
BigCubeCat <bigcubecat@yandex.ru>
2023-02-02 14:42:36 +0700

    update Pizza items

---
+++ src/components/Pizza/PizzaItem.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  Box, Typography
+} from '@mui/material';
+
+import { TIngredient } from './ingredients';
+
+export default function PizzaItem({ item }: {
+  item: TIngredient
+}) {
+  return (
+    <Box>
+      <img
+        width="80px"
+        height="80px"
+        src={item.img}
+        sx={{
+          transform: "rotate(180deg)"
+        }}
+      />
+      <Typography color="#fff" variant="h6" component="div" >
+        {item.title}
+      </Typography>
+    </Box>
+  );
+}
---
+++ src/components/Pizza/ingredients.ts
@@ -0,0 +1,38 @@
+export type TIngredient = {
+  title: string;
+  img?: string;
+}
+export const PIZZA_INGREDIENTS: TIngredient[] = [
+  {
+    title: "Olives",
+    img: "/img/ingredients/olives.png",
+  },
+  {
+    title: "Cheese",
+    img: "/img/ingredients/cheese.png",
+  },
+  {
+    title: "Pepper",
+    img: "/img/ingredients/pepper.png",
+  },
+  {
+    title: "Pepperoni",
+    img: "/img/ingredients/pepperoni.png",
+  },
+  {
+    title: "Bacon",
+    img: "/img/ingredients/bacon.png",
+  },
+  {
+    title: "Chicken",
+    img: "/img/ingredients/chicken.png",
+  },
+  {
+    title: "Beef",
+    img: "/img/ingredients/beef.png",
+  },
+  {
+    title: "Sausages",
+    img: "/img/ingredients/sausages.png",
+  },
+]
