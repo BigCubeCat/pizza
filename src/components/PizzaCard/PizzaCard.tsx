4712eca79b5580beffde4da52184f0a28a7f026c
BigCubeCat <bigcubecat@yandex.ru>
2023-02-02 18:59:27 +0700

    fix PizzaCard

--- src/components/PizzaCard/PizzaCard.tsx
+++ src/components/PizzaCard/PizzaCard.tsx
@@ -1,32 +1,70 @@
 import React from 'react';
 
 import {
-  Card, CardContent, Typography, CardMedia
+  Card, CardContent, Typography, Box, Button
 } from "@mui/material";
 import { TPizza } from '../../types/pizza';
 
 export default function PizzaCard({ pizza }: { pizza: TPizza }) {
-  console.log(pizza)
   return (
     <Card sx={{
       maxWidth: 300, margin: 5,
+      display: "flex", justifyContent: "space-between",
+      flexDirection: "column", padding: 1
     }}>
       <img
         height="200px"
         width="200px"
         src={pizza.img}
-        alt="green iguana"
+        alt={pizza.name}
+        style={{ alignSelf: "center" }}
       />
       <CardContent>
-        <Typography gutterBottom variant="h6" component="div">
-          {pizza.name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
+        <Box sx={{
+          display: 'flex', justifyContent: 'space-between',
+          paddingRight: 1, paddingLeft: 1,
+        }}>
+          <Typography gutterBottom
+            variant="h6"
+            component="div"
+            textAlign={"start"}
+            fontWeight="bold"
+          >
+            {pizza.name}
+          </Typography>
+          <Typography gutterBottom
+            component="div"
+            textAlign={"start"}
+            color="text.secondary"
+          >
+            {pizza.price.size.medium}см
+          </Typography>
+        </Box>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          textAlign="start"
+        >
           {
             pizza.ingredients.join(', ')
           }
         </Typography>
       </CardContent>
+      <Box sx={{
+        display: 'flex', justifyContent: 'space-between',
+        paddingRight: 1, paddingLeft: 1, marginTop: 2,
+      }}>
+        <Typography fontWeight="bold">{pizza.price.default}₽</Typography>
+        <Button
+          variant="contained"
+          sx={{
+            borderRadius: 100,
+            color: "#fff",
+            fontWeight: "bold"
+          }}
+          onClick={() => {}}
+        >Добавить</Button>
+      </Box>
     </Card>
   )
 }
